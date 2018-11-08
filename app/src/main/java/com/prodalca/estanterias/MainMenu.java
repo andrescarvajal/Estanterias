08a5466eb2dbc707ab631190a32439b5edbb749a
Andres Carvajal <andrescarvajal9113@hotmail.com>
2018-11-07 21:21:22 -0500

    including existing file from old project

--- app/src/main/java/com/prodalca/estanterias/MainMenu.java
+++ app/src/main/java/com/prodalca/estanterias/MainMenu.java
@@ -3,11 +3,53 @@ package com.prodalca.estanterias;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainMenu extends AppCompatActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.NumberPicker;
+import android.view.View.OnClickListener;
 
+public class MainMenu extends Activity implements OnClickListener {
+
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    public void menuEstCanastillas (View view){
+
+        Intent intent = new Intent(this, CalcularEstanteriaCanastillas.class);
+        //EditText editText = (EditText) findViewById(R.id.etNum1);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
+    public void menuEstCarga (View view){
+
+        Intent intent = new Intent(this, CalcularEstanteriaCarga.class);
+        //EditText editText = (EditText) findViewById(R.id.etNum1);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
 }
